45219330ee420039af5b88e9b8c4faf0f51690a1
Wrik Das <wrikdas22@gmail.com>
2021-11-11 19:41:37 +0530

    Simplified code

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,27 +1,12 @@
-LETTERS = "abcdefghijklmnopqrstuvwxyz"
-
-def shift_character(character, shift_factor)
-  unless character.between?('a', 'z') || character.between?('A', 'Z')
-    return character
-  end
-
-  is_upper = character.between?('A', 'Z') ? true : false
-  base = is_upper ? 65 : 97
-
-  index = character.ord % base
-  shifted_index = index + shift_factor
-
-  shifted_index = 26 - shifted_index.abs if shifted_index < 0  # right shift wrap
-  shifted_index = shifted_index % 26 if shifted_index >= 26    # left  shift wrap
-
-  new_char = LETTERS[shifted_index]
-  is_upper ? new_char.upcase : new_char
-end
-
-def caesar_cipher(string, shift_factor = 5)
+def caesar_cipher(string, shift_factor)
   cipher = ""
   string.each_char do |character|
-    cipher += shift_character(character, shift_factor)
+    if character.between?("A", "Z") || character.between?("a", "z")
+      base = character.between?("A", "Z") ? 65 : 97
+      cipher += ((((character.ord % base) + shift_factor) % 26) + base).chr
+    else
+      cipher += character
+    end
   end
   cipher
 end
